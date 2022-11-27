4d7cc5ad06e704b61f3a9dcf0e322e835c95cdcd
Keila Lopez <keilalofra@gmail.com>
2022-11-27 13:19:07 -0600

    first and last commit:

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :name
+      # the id column is generated automatically 
+      #for every table! no need to specify it here
+    end
   end
 end
